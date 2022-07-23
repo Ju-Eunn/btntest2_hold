043d26e9c8d9ba4932b1743de066dcdeb4d5a20f
Ju-Eunn <cje4893@naver.com>
2022-07-23 23:09:52 +0900

    버튼 누른 채로 유지

--- app/src/main/java/com/example/btntest2_hold/MainActivity.kt
+++ app/src/main/java/com/example/btntest2_hold/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.btntest2_hold
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn1 : ImageButton = findViewById<ImageButton>(R.id.btn1)
+        btn1.setOnClickListener {
+            btn1.isSelected = btn1.isSelected != true
+        }
     }
 }
